1f20c2acc3f093ed37a77d389eafccfae362f985
yunfenger <yahoochemical@gmail.com>
2017-09-03 22:09:40 +0800

    get price for the id and get the avilable

--- amzon.py
+++ amzon.py
@@ -68,12 +68,10 @@ GOOD_PRICE=50
 # a = brw.find_element_by_xpath("//li[@data-defaultasin='B01GDZSZ6Q']")
 # B00LTPUMJ4 ======>12*600g
 # a = brw.find_element_by_xpath("//li[@data-defaultasin='B00LTPUMJ4']")
-
-
 ######################### the file operation ###########################################################################
 def log(str):
     fo = open(log_file, 'a')
-    fo.write(str+'\n')
+    fo.write(str+'\t'+datetime.now().strftime('%d_%H:%M:%S')+'\n')
     fo.close()
 
 def GetUserFromFile(count):
@@ -115,8 +113,8 @@ def login(username,passwd):
 # return 49.88 else return -1
 def getprice(str):
     #'5 x 600g\nEUR 49.88 (EUR 16.63 / kg)'
-    print(str)
-    index = str.find('\nEUR', 0, len(str))
+    #print(str)
+    index = str.find('EUR', 0, len(str))
     if(index==-1):
         return -1
     index2= str.find('(EUR', 0, len(str))
@@ -125,49 +123,32 @@ def getprice(str):
     price= float(str[index+4:index2])
     return price
 
-def check_goods():
-    status = False
-    try:
-        #brw.get("https://www.amazon.de/dp/B01GDZSZ6Q/ref=twister_B013USRV1E?_encoding=UTF8&th=1")
-        brw.get(GOOD_URI)
-        sleep(5)
-        # get the element of the data  5*600g
-        a = brw.find_element_by_xpath("//li[@data-defaultasin='B01GDZSZ6Q']")
-        #a.click();
-        price = getprice(a.text)
-        #log(a.text)
-        log('price:' + str(price))
-        a.click()
-        # check the price
-        if((price>0)&(price<50.00)):
-            status= True
-        else:
-            status=False
-    except Exception as e:
-        print(traceback.print_exc())
-        status= False
-    return status
-
+def getprice2(str):
+    price=-1
+    index = str.find('EUR', 0, len(str))
+    if(index==-1):
+        pass
+    else:
+        price= float(str[index+4:index+10])
+    return price
 
 def check_goods_2(url, Goodstr,exp_price):
-    status = False
+    status = 0
     price =-1
     pricetest=''
     try:
-        #brw.get("https://www.amazon.de/dp/B01GDZSZ6Q/ref=twister_B013USRV1E?_encoding=UTF8&th=1")
         brw.get(url)
-        sleep(5)
+        sleep(1)
         # get the element of the data  5*600g
         a = brw.find_element_by_xpath("//li[@data-defaultasin='"+Goodstr+"']")
-        #a.click();
-        pricetest=a.text
-        price = getprice(a.text)
+        pricetest=a.text[a.text.find('\n')+1:len(a.text)]
+        price = getprice(pricetest)
         a.click()
     except Exception as e:
-        print(traceback.print_exc())
-        status= False
+        pricetest='not found the element'+Goodstr
     # if the good is ok, submit the
     if ((price > 0) & (price <= exp_price)):
+        brw.save_screenshot('A'+datetime.now().strftime('%Y-%m-%d_%H_%M_%S')+Goodstr + ".png")
         try:
             process = 2;
             a = brw.find_element_by_id('one-click-button')
@@ -199,17 +180,80 @@ def check_goods_2(url, Goodstr,exp_price):
             print(brw.title)
             if(brw.title=='Amazon Sign In'):
                 login(username, passwd)
-            brw.save_screenshot(datetime.now().strftime('%Y-%m-%d %H:%M:%S') +"_login"+ ".png")
+            brw.save_screenshot('B'+datetime.now().strftime('%Y-%m-%d_%H_%M_%S') +"_login"+ ".png")
             a = brw.find_element_by_xpath("//input[@name='continue-bottom']")
             a.click()
-            status = True
+            status = 1
         except Exception as e:
             print(traceback.print_exc())
-            status = False
-
+            status = 0
     return (status,price,pricetest)
 
 
+def check_goods_3(url, Goodstr,exp_price):
+    status = 0
+    price =-1
+    pricetest=''
+    try:
+        brw.get(url)
+        sleep(1)
+        # get the element of the data  5*600g
+        a = brw.find_element_by_id('availability')
+        pricetest = a.text
+        #print(a.text)
+        if(a.text.find('in stock')!=-1):
+            b = brw.find_element_by_id('price_feature_div')
+          #  print('in stock'+b.text)
+            price =getprice2(b.text)
+        else:
+            b = brw.find_element_by_id('olp_feature_div')
+           # print('a'+b.text)
+            price = getprice2(b.text)
+    except Exception as e:
+        pricetest='not found the element'+Goodstr
+        status= 0
+    # if the good is ok, submit the
+    if ((price > 0) & (price <= exp_price)):
+        brw.save_screenshot('A'+datetime.now().strftime('%Y-%m-%d_%H_%M_%S')+Goodstr + ".png")
+        try:
+            process = 2;
+            a = brw.find_element_by_id('one-click-button')
+            a.click()
+            if (brw.title == 'Amazon Sign In'):
+                login(username,passwd)
+            process = 3;
+            a = brw.find_element_by_xpath("//input[@name='continue-bottom']")
+            a.click()
+            book_status = True
+            return
+        except Exception as e:
+            #print(traceback.print_exc())
+            if(process==2):
+                print('can not find the one-click-button')
+            if(process==3):
+                print('can not find the continue-bottom')
+            book_status = False
+        try:
+            #a=brw.find_element_by_id('oneClickSignIn')
+            a = brw.find_element_by_xpath("//a[@class='oneClickSignInLink']")
+            a.click()
+            if (brw.title == 'Amazon Sign In'):
+                login(username,passwd)
+            #one - click - button
+            a = brw.find_element_by_id('one-click-button')
+            a.click()
+            title = brw.title
+            print(brw.title)
+            if(brw.title=='Amazon Sign In'):
+                login(username, passwd)
+            brw.save_screenshot('B'+datetime.now().strftime('%Y-%m-%d_%H_%M_%S') +"_login"+ ".png")
+            a = brw.find_element_by_xpath("//input[@name='continue-bottom']")
+            a.click()
+            status = 1
+        except Exception as e:
+            print(traceback.print_exc())
+            status = 0
+    return (status,price,pricetest)
 ########################################################################################################################
 ###########################----------end-----################################################################
 ########################################################################################################################
@@ -234,99 +278,35 @@ def book2():
 
     while book_status ==False:
         if(good_status.GOOD_ATAMI1_STATUS==0):
-            url=GOOD_URI
+            url='https://www.amazon.de/dp/B01GDZSZ6Q/ref=twister_B013USRV1E?_encoding=UTF8&th=1&language=en_GB'
             goodstr='B01GDZSZ6Q'
             exp_price=49.88
-            [good_status.GOOD_ATAMI1_STATUS,price,priceT]=check_goods_2(url,goodstr,exp_price)
+            [good_status.GOOD_ATAMI1_STATUS,price,priceT]=check_goods_3(url,goodstr,exp_price)
+            print('GOOD_ATAMI1_STATUS:'+str(good_status.GOOD_ATAMI1_STATUS)+'\tprice:'+str(price)+'\t'+priceT)
+            log('GOOD_ATAMI1_STATUS:' + str(good_status.GOOD_ATAMI1_STATUS) + '\tprice:' + str(price) +'\t'+ priceT)
+
 
         if(good_status.GOOD_ATAMI2_STATUS==0):
-            url='https://www.amazon.de/Aptamil-Kindermilch-Jahren-4er-Pack/dp/B00BSNAC9C/'
+            url='https://www.amazon.de/Aptamil-Kindermilch-Jahren-4er-Pack/dp/B00BSNAC9C/?language=en_GB'
+            #'https://www.amazon.de/Aptamil-Kindermilch-Jahren-4er-Pack/dp/B00BSNAC9C/?language=en_GB'
             goodstr='B00BSNAC9C'
             exp_price=49.75
-            [good_status.GOOD_ATAMI2_STATUS, price, priceT] = check_goods_2(url, goodstr, exp_price)
+            [good_status.GOOD_ATAMI2_STATUS, price, priceT] = check_goods_3(url, goodstr, exp_price)
+            print('GOOD_ATAMI2_STATUS:' + str(good_status.GOOD_ATAMI2_STATUS) + '\tprice:' + str(price)+'\t' + priceT)
+            log('GOOD_ATAMI2_STATUS:' + str(good_status.GOOD_ATAMI2_STATUS) + '\tprice:' + str(price) +'\t'+ priceT)
 
         if(good_status.GOOD_ATAMIProfutura_STATUS==0):
             url='https://www.amazon.de/Aptamil-Profutura-Folgemilch-nach-Monat/dp/B016WEDI6K?language=en_GB'
-            goodstr='B01GDZSZ6Q'
+            goodstr='B016WEDI6K'
             exp_price=77.92
-            [good_status.GOOD_ATAMIProfutura_STATUS, price, priceT] = check_goods_2(url, goodstr, exp_price)
+            [good_status.GOOD_ATAMIProfutura_STATUS, price, priceT] = check_goods_3(url, goodstr, exp_price)
+            print('GOOD_ATAMIPF_STATUS:' + str(good_status.GOOD_ATAMIProfutura_STATUS) + '\tprice:' + str(price)+'\t' + priceT)
+            log('GOOD_ATAMIPF_STATUS:' + str(good_status.GOOD_ATAMIProfutura_STATUS) + '\tprice:' + str(
+                price) + '\t'+priceT)
 
-        if((good_status.GOOD_ATAMI1_STATUS==0)|(good_status.GOOD_ATAMIProfutura_STATUS==0)|(good_status.GOOD_ATAMI2_STATUS==0)):
-            book_status=False
+        if((good_status.GOOD_ATAMI1_STATUS==1)&(good_status.GOOD_ATAMIProfutura_STATUS==1)&(good_status.GOOD_ATAMI2_STATUS==1)):
+            book_status=True
     return True
-
-
-def book():
-    print('book operation')
-    # the four data-defaultasin like
-    # data_index={ B00CMLT4QQ,B00BSNACII,B01GDZSZ6Q,B00LTPUMJ4}
-    book_status=False
-    good_status=Good_Status()
-    process=0
-    a=False
-    count=0
-    time=5
-    while book_status ==False:
-        while check_goods()==False:
-            log('check the goods num'+str(count)+'at time \t'+datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            count=count+1
-            sleep(1)
-        #a = brw.find_element_by_xpath("//a[@class='oneClickSignInLink']")
-        brw.save_screenshot(str(count)+"_count.png")
-        try:
-            process = 1
-            a = brw.find_element_by_xpath("//li[@data-defaultasin='B01GDZSZ6Q']")
-            a.click()
-        except Exception as e:
-            print('can not find the goods B01GDZSZ6Q ')
-            book_status = False
-            continue
-        try:
-            process = 2;
-            a = brw.find_element_by_id('one-click-button')
-            a.click()
-            process = 3;
-            a = brw.find_element_by_xpath("//input[@name='continue-bottom']")
-            a.click()
-            book_status = True
-            return
-        except Exception as e:
-            #print(traceback.print_exc())
-            if(process==2):
-                print('can not find the one-click-button')
-
-            if(process==3):
-                print('can not find the continue-bottom')
-            book_status = False
-
-        try:
-            #a=brw.find_element_by_id('oneClickSignIn')
-            a = brw.find_element_by_xpath("//a[@class='oneClickSignInLink']")
-            a.click()
-            if (brw.title == 'Amazon Sign In'):
-                login(username,passwd)
-            #one - click - button
-            a = brw.find_element_by_id('one-click-button')
-            a.click()
-            title = brw.title
-            print(brw.title)
-            if(brw.title=='Amazon Sign In'):
-                login('yongyaqin1225@126.com','yongyaqin1225')
-        # check the Select shipping address
-
-            # submit the order
-            brw.save_screenshot(str(count) +"_login"+ "_count.png")
-            a = brw.find_element_by_xpath("//input[@name='continue-bottom']")
-            a.click()
-            book_status = True
-        except Exception as e:
-            print(traceback.print_exc())
-            book_status = False
-        # check the order is OK
-
-    return True
-
-
 
 if __name__ == "__main__":
     # get the user info from the usr txt
@@ -336,10 +316,14 @@ if __name__ == "__main__":
     log('amzon_start'+datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
     global brw
     brw = webdriver.Chrome()
-    brw.implicitly_wait(3)  # seconds
-    brw.set_window_size(1366, 768)
+    brw.implicitly_wait(1)  # seconds
+    #brw.set_window_size(1366, 768)
     brw.get(LOGIN_URI)
-    sleep(10)
+    #sleep(2)
+    #pngfile=datetime.now().strftime('%Y-%m-%d_%H_%M_%S')+'.png'
+    #print(pngfile)
+    #brw.save_screenshot(pngfile)
+    #sleep(3)
     status =False
     cnt =0;
     # while(status == False):
@@ -364,4 +348,4 @@ if __name__ == "__main__":
     # #status=True
     #     print("login_"+str(status))
     status = book2()
-    log('the book result'+status)
+    log('the book result'+str(status))
